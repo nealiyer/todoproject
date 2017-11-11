f90cecd51001fb71605f7ad205e424a0d0a4950e
Neal Iyer <the.neal.returns@gmail.com>
2017-11-12 01:04:19 +0400

    added validation

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _= require('underscore');
+
 var app = express();
 
 var PORT = process.env.PORT || 3000;
@@ -19,19 +21,14 @@ app.get('/todos', function(req, res){
 app.get('/todos/:id', function(req,res)
 {
 	var todoid = parseInt(req.params.id, 10);
-	var flag;
+	var flag = _.findWhere(todos, {id: todoid});
 
-	todos.forEach(function(todo){
-		if(todo.id === todoid)
-		{
-			flag = todo;
-		}
-	});
+	
 		
 
 		if(flag)
 		{
-			res.json(flag)
+			res.json(flag);
 		}
 		else
 		{
@@ -44,7 +41,11 @@ app.get('/todos/:id', function(req,res)
 
 app.post('/todos', function(req,res)
 {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+	if(!_.isBoolean(body.completed)|| !_.isString(body.description)|| body.description.trim().length ===0){
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
 	body.id = todoNextId;
 	todos.push(body);
 	todoNextId+=1;
